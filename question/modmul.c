b1420aeff28bff460de2ef7fe1bcf741b0eb3d23
Jamie Luxford <jl15219@my.bristol.ac.uk>
2018-02-12 20:30:37 +0000

    Wind Exp Working

--- question/modmul.c
+++ question/modmul.c
@@ -8,6 +8,9 @@
 #include "modmul.h"
 #include <time.h>
 
+// NEEDS TO BE BIGGER THAN 4 FOR NO REASON
+const int windExpK = 8;
+
 /* Perform stage 1:
  *
  * - read each 3-tuple of N, e and m from stdin,
@@ -15,66 +18,77 @@
  * - write the ciphertext c to stdout.
  */
 
-void windExp(mpz_t t, mpz_t x, mpz_t y2, mpz_t n, int k) {
-  mpz_t y,temp;
+void windExp(mpz_t t, mpz_t x, mpz_t y, mpz_t n, int k) {
+  //mpz_t y;
   mpz_set_ui(t,1);
-  mpz_init(y);
-  mpz_init(temp);
-  mpz_mul(y,y2,t);
-
+  //mpz_init(y);
+  //mpz_mul(y,y2,t);
   //gmp_printf("23: %Zd\n",y);
 
   int j = 2;
-  j = j << (k-1);
+  j = j << (k-2);
 
   mpz_t T [j + 2];
-  mpz_init_set_ui(T[0],1);
-
+  mpz_init_set(T[0],x);
   for (mp_bitcnt_t i = 0; i < (j + 2); i ++) {
     mpz_init(T[i+1]);
     mpz_mul(T[i+1],T[i],x);
+    mpz_mul(T[i+1],T[i+1],x);
     mpz_mod(T[i+1],T[i+1],n);
   }
 
-
-  mp_bitcnt_t i = (mpz_sizeinbase(y2,2));
+  mp_bitcnt_t i = (mpz_sizeinbase(y,2));
 
   mp_bitcnt_t l;
   mp_bitcnt_t u;
-  mp_bitcnt_t temp;
 
-  while (i >= 0) {
+  while ((i + 1) > 0)  {
+    //gmp_printf("%Zd\n", t);
     if (mpz_tstbit(y,i) == 0) {
       l = i;
       u = 0;
     }
     else {
-      if (i-k+1 > 0) {l = i - k + 1;}
+      j = i-k+1;
+      if (j > 0) {l = i - k + 1;}
       else {l = 0;}
+      //gmp_printf("%d",l);
       u = 0;
-      while (mpz_tstbit(y,l) == 0) {l = l + 1;}
+      while (mpz_tstbit(y,l) == 0) {
+        l = l + 1;
+        //gmp_printf(", %d", l);
+      }
       //mpz_mul(temp,y,1);
       //for (int a = 0, a < l, a) {mpz};
-      // GRAB THE REQUIRED NUMBER
-      //while (temp >= l) {
-      //  u = u*2;
-      //  if (mpz_tstbit(y,temp) == 1) {u = u + 1;}
-      //  temp = temp - 1;
-      //}
+      for (j = i; (j + 1) > l; j --) {
+        u = u << 1 ;
+        u = u + mpz_tstbit(y,j);
+      }
     }
 
-    for (int j = i - l + 1; j > 0; j --) {mpz_mul(t,t,t);}
+    for (int j = i - l + 1; j > 0; j --) {
+      mpz_mul(t,t,t);
+      mpz_mod(t,t,n);
+    }
 
-    if (u != 0) {mpz_add(t,t,T[(int)((u - 1)/2)]);}
+    if (u != 0) {
+      mpz_mul(t,t,T[(int)((u - 1)/2)]);
+      mpz_mod(t,t,n);
+    }
     i = l - 1;
   }
-  mpz_clear(y);
+  for (mp_bitcnt_t i = 0; i < (j + 3); i ++) {
+    mpz_clear(T[i]);
+  }
 }
 
-void stage1(mpz_t c, mpz_t n, mpz_t e, mpz_t m) {
-  mpz_powm_sec(c,m,e,n);
-  // fill in this function with solution
+//////////////////////////////////////
 
+void stage1(mpz_t c, mpz_t n, mpz_t e, mpz_t m) {
+  windExp(c,m,e,n,windExpK);
+  //mpz_powm_sec(c,m,e,n);
+  // fill in this function with solution
+  gmp_printf("%ZX\n",c);
 }
 
 /* Perform stage 2:
@@ -85,9 +99,39 @@ void stage1(mpz_t c, mpz_t n, mpz_t e, mpz_t m) {
  */
 
 void stage2(mpz_t m, mpz_t n, mpz_t d, mpz_t p, mpz_t q, mpz_t d_p, mpz_t d_q, mpz_t i_p, mpz_t i_q, mpz_t c) {
-  mpz_powm_sec(m,c,d,n);
-  // fill in this function with solution
+  mpz_t m1,m2;
+  mpz_init(m1);
+  mpz_init(m2);
 
+  windExp(m1,c,d_p,p,windExpK);
+  windExp(m2,c,d_q,q,windExpK);
+
+  //i_p = p^-1 (mod q)
+  //i_q = q^-1 (mod p)
+
+  //if (mpz_cmp(p,q) < 0) {
+    mpz_sub(m,m2,m1);
+    mpz_mul(m,m,i_p);
+    mpz_mod(m,m,n);
+    mpz_mul(m,m,p);
+    mpz_add(m,m,m1);
+    mpz_mod(m,m,n);
+  //}
+  //else {
+  //  mpz_sub(m,m1,m2);
+  //  mpz_mul(m,m,i_q);
+  //  mpz_mod(m,m,n);
+  //  mpz_mul(m,m,q);
+  //  mpz_add(m,m,m2);
+  //  mpz_mod(m,m,n);
+  //}
+
+
+  gmp_printf("%ZX\n",m);
+
+  //windExp(m,c,d,n,windExpK);
+  // fill in this function with solution
+  //gmp_printf("%ZX\n",m);
 }
 
 /* Perform stage 3:
@@ -114,11 +158,12 @@ void stage3(mpz_t c1, mpz_t c2, mpz_t p, mpz_t q, mpz_t g, mpz_t h, mpz_t m) {
   gmp_printf("%ZX from %ZX\n",r,q);
 
   mpz_mod(r,r,q);
-  mpz_powm_sec(c1,g,r,p);
-  mpz_powm_sec(c2,h,r,p);
+  windExp(c1,g,r,p,windExpK);
+  windExp(c2,h,r,p,windExpK);
   mpz_mul(c2,c2,m);
   mpz_mod(c2,c2,p);
   // fill in this function with solution
+  gmp_printf("%ZX\n%ZX\n",c1,c2);
   mpz_clear(r);
 }
 
@@ -135,11 +180,11 @@ void stage4(mpz_t m, mpz_t p, mpz_t q, mpz_t g, mpz_t x, mpz_t c1, mpz_t c2) {
   mpz_init(e);
   mpz_mul_si(e,x,-1);
   mpz_mod(e,e,q);
-  mpz_powm_sec(m,c1,e,p);
+  windExp(m,c1,e,p,windExpK);
   mpz_mul(m,m,c2);
   mpz_mod(m,m,p);
   // fill in this function with solution
-
+  gmp_printf("%ZX\n",m);
 }
 
 /* The main function acts as a driver for the assignment by simply invoking the
@@ -171,12 +216,11 @@ int main( int argc, char* argv[] ) {
 
     stage1(c,n,e,m);
 
-    gmp_printf("%ZX\n",c);
-
     mpz_clear(n);
     mpz_clear(e);
     mpz_clear(m);
     mpz_clear(c);
+
   }
   else if( !strcmp( argv[ 1 ], "stage2" ) ) {
     mpz_t n, d, p, q, d_p, d_q, i_p, i_q, c, m;
@@ -222,8 +266,6 @@ int main( int argc, char* argv[] ) {
 
     stage2(m,n,d,p,q,d_p,d_q,i_p,i_q,c);
 
-    gmp_printf("%ZX\n",m);
-
     mpz_clear(n);
     mpz_clear(d);
     mpz_clear(p);
@@ -265,8 +307,6 @@ int main( int argc, char* argv[] ) {
 
     stage3(c1,c2,p,q,g,h,m);
 
-    gmp_printf("%ZX\n%ZX\n",c1,c2);
-
     mpz_clear(p);
     mpz_clear(q);
     mpz_clear(g);
@@ -306,8 +346,6 @@ int main( int argc, char* argv[] ) {
 
     stage4(m,p,q,g,x,c1,c2);
 
-    gmp_printf("%ZX\n",m);
-
     mpz_clear(m);
     mpz_clear(p);
     mpz_clear(q);
@@ -318,14 +356,32 @@ int main( int argc, char* argv[] ) {
   }
   else if( !strcmp( argv[ 1 ], "test" ) ) {
     mpz_t t,x,y,n;
-    mpz_init_set_ui(t,1);
-    mpz_init_set_ui(x,1234);
-    mpz_init_set_ui(y,3094);
-    mpz_init_set_ui(n,503*509);
-    int k = 4;
+    mpz_init(t);
+    mpz_init(x);
+    mpz_init(y);
+    mpz_init(n);
+    //mpz_set_ui(t,1);
+    //mpz_set_ui(x,1691348502958209058);
+    //mpz_set_ui(y,1134582093582034421);
+    //mpz_set_ui(n,9252522347);
 
+    if (1 != gmp_scanf("%ZX\n",x)) {
+      abort();
+    }
+    if (1 != gmp_scanf("%ZX\n",y)) {
+      abort();
+    }
+    if (1 != gmp_scanf("%ZX\n",n)) {
+      abort();
+    }
+
+    gmp_printf("Finished Reading in\n");
+
+    int k = 8;
     windExp(t,x,y,n,k);
-
+    gmp_printf("Wind Exp gives: %Zd\n", t);
+    mpz_powm_sec(t,x,y,n);
+    gmp_printf("Result should be: %Zd\n", t);
     mpz_clear(t);
     mpz_clear(x);
     mpz_clear(y);
